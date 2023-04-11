88214170d50275a2f6f2e88949a951076300d3b6
ConorOSullivan <cosullivan66@gmail.com>
2023-04-10 20:30:04 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import Head from 'next/head';
+import React from 'react';
 import styles from '../styles/Home.module.css';
-import tanShape from '../styles/Shape.module.css';
-import stripesShape from '../styles/Stripes.module.css';
-
 
 const Home = () => {
-  const [menuVisible, setMenuVisible] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuVisible(!menuVisible);
-  };
-
-  const RSVPLink = "https://docs.google.com/forms/d/e/1FAIpQLSeFIOqKB-TlsehdTna6vVWYxmyVO7gbQg3517Ms1CEwJOCnlw/viewform";
-  const IGLink = "https://www.instagram.com/reel/CdhUlP6IZ_x/?igshid=YmMyMTA2M2Y=";
-
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Next.js Project</title>
-        <meta name="description" content="Next.js project with a custom background color and diagonal separation" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <div className={tanShape.shape}></div>
-      <div className={stripesShape.stripe3}></div>
-      <div className={stripesShape.stripe2}></div>
-      <div className={stripesShape.stripe1}></div>
-      
-      <img src="/OutsideManzLogo.svg" alt="Outside Manz Logo" className={styles.logo} />
-
-      <div className={styles.hamburgerMenu} onClick={toggleMenu}>
-        <div className={styles.hamburgerIcon}></div>
-        <ul className={`${styles.menuList} ${menuVisible ? styles.visible : ''}`}>
-          <li className={styles.menuItem}><a href={RSVPLink}>RSVP Here</a></li>
-          <li className={styles.menuItem}><a href={IGLink}>Get Stoked</a></li>
-          <li className={styles.menuItem}>Lineup - Coming Soon</li>
-        </ul>
+      <div className={styles.blackBox}>
+        <h1 className={styles.title}>Blind Tasting Bonanza</h1>
+        <div className={styles.description}>
+          Where: 518 Arenas
+          <br />
+          When: 6pm Saturday April 15th
+          <br />
+          Food: Charcuterie and Dinner
+          <br />
+          <br />
+          As some of the biggest wine snobs I know, you are cordially invited to a night of tasting, guessing, and spitting (jk WE DON’T SPIT) some fantastic and mysterious wines. Each couple will bring a bottle of something you’re not super familiar with but have been curious about. No explicit dollar range here but make it nice. If you bring two buck chuck and we all get conned into picking it as our favorite, instant expulsion from wine club.
+          <br />
+          <br />
+          To do ahead of the party:
+          <br />
+              - Wrap your bottle in something and cut the top foil away (to keep it anonymous OF COURSE). The cork may be labeled but we’ll just try to ignore that.
+              <br />
+              - Fill out <a href="https://example.com/doc" className={styles.hyperlink}>this doc</a>
+          <br />
+          <br />
+          The setup:
+          <br />
+          - Wines will be poured into labeled carafes, and we’ll note which couple’s wine is in which
+          <br />
+          - Everyone will have access to the doc which will note all of the wines present
+          <br />
+          - We drink
+          <br />
+          - We note
+          <br />
+          <br />
+          We’re going to be judging:
+          <br />
+          - Match the carafe to the bottle
+          <br />
+          - Select your favorite wine
+          <br />
+          - Write your tasting notes. Points if your notes match those in the doc.
+          <br />
+          <br />
+          Winner gets a special bottle. Loser does a wine bong.
+        </div>
       </div>
-      <a href={RSVPLink}>
-        <button className={styles.rsvpButton}>RSVP here</button>
-      </a>
-
-      <img src="/EventDate.svg" alt="Event Date" className={styles.eventDate} />
-      
     </div>
-  
   );
 };
 
